92a99bb855c241128d73be3acf5aa3ffd06431c5
ograh <bernard.ograh@amalitech.com>
2026-01-13 11:36:17 +0000

    first commit

---
+++ src/main/java/org/amalitech/bloggingplatformspring/BloggingPlatformSpringApplication.java
@@ -0,0 +1,13 @@
+package org.amalitech.bloggingplatformspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingPlatformSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingPlatformSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/amalitech/bloggingplatformspring/BloggingPlatformSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amalitech.bloggingplatformspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
