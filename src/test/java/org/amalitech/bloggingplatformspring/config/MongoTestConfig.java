dedf2f0056e47e2c3b082af3d57b7df48708869a
ograh <bernard.ograh@amalitech.com>
2026-01-19 10:27:38 +0000

    feat: Add MongoDB connection configuration and test classes for database integration

---
+++ src/test/java/org/amalitech/bloggingplatformspring/config/MongoTestConfig.java
@@ -0,0 +1,6 @@
+package org.amalitech.bloggingplatformspring.config;
+
+public class MongoTestConfig {
+    public static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    public static final String DATABASE_NAME = "blog_db_test";
+}
