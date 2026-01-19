75d57840df573f493798ce2d0936c16c72a011be
ograh <bernard.ograh@amalitech.com>
2026-01-19 11:49:30 +0000

    feat: Add GraphQL types for creating and deleting comments

---
+++ src/main/java/org/amalitech/bloggingplatformspring/graphql/types/DeleteCommentInput.java
@@ -0,0 +1,10 @@
+package org.amalitech.bloggingplatformspring.graphql.types;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteCommentInput {
+    private String authorId;
+}
---
+++ src/main/java/org/amalitech/bloggingplatformspring/graphql/types/GraphQLComment.java
@@ -0,0 +1,19 @@
+package org.amalitech.bloggingplatformspring.graphql.types;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GraphQLComment {
+    private String id;
+    private Integer postId;
+    private String author;
+    private String authorUsername;
+    private String content;
+    private LocalDateTime createdAt;
+}
