64f7bb7525b42725a303ee36c6695aad7fade755
ograh <bernard.ograh@amalitech.com>
2026-01-22 15:35:28 +0000

    feat: Remove deprecated database connection classes and DAOs to streamline configuration

--- src/main/java/org/amalitech/bloggingplatformspring/config/ConnectionProvider.java
+++
@@ -1,8 +0,0 @@
-package org.amalitech.bloggingplatformspring.config;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionProvider {
-    Connection getConnection() throws SQLException;
-}
