fb119d9446e87d7d83c39f49c6c05edc45f7e884
ograh <bernard.ograh@amalitech.com>
2026-01-23 10:50:46 +0000

    feat: Refactor utility classes to enhance code structure and readability by making methods static and final

--- src/main/java/org/amalitech/bloggingplatformspring/utils/UserUtils.java
+++ src/main/java/org/amalitech/bloggingplatformspring/utils/UserUtils.java
@@ -3,9 +3,9 @@ package org.amalitech.bloggingplatformspring.utils;
 import org.amalitech.bloggingplatformspring.dtos.responses.UserResponseDTO;
 import org.amalitech.bloggingplatformspring.entity.User;
 
-public class UserUtils {
+public final class UserUtils {
 
-    public UserResponseDTO mapUserToUserResponse(User user) {
+    public static UserResponseDTO mapUserToUserResponse(User user) {
         UserResponseDTO userResponseDTO = new UserResponseDTO();
         userResponseDTO.setId(String.valueOf(user.getId()));
         userResponseDTO.setUsername(user.getUsername());
