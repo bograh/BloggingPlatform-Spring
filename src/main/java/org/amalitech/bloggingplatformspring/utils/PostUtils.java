9e78bb913857b3ba31c66cfbbcfba25f5716a97d
ograh <bernard.ograh@amalitech.com>
2026-01-23 09:28:14 +0000

    feat: Refactor PostResponseDTO and PostUtils to include authorId and postedAt fields for enhanced post response structure

--- src/main/java/org/amalitech/bloggingplatformspring/utils/PostUtils.java
+++ src/main/java/org/amalitech/bloggingplatformspring/utils/PostUtils.java
@@ -11,52 +11,23 @@ import org.amalitech.bloggingplatformspring.repository.CommentRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.sql.Array;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 public class PostUtils {
 
-    public Post mapRowToPost(ResultSet rs) throws SQLException {
-        int id = rs.getInt("id");
-        String title = rs.getString("title");
-        String body = rs.getString("body");
-        LocalDateTime createdAt = rs.getTimestamp("posted_at").toLocalDateTime();
-        LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
-        UUID authorId = (UUID) rs.getObject("author_id");
-
-        return new Post(
-                (long) id, title, body,
-                null, createdAt, updatedAt, null
-        );
-    }
-
-    public PostResponseDTO mapRowToPostResponse(ResultSet rs, Long totalComments) throws SQLException {
-        int id = rs.getInt("id");
-        String title = rs.getString("title");
-        String body = rs.getString("body");
-        String author = rs.getString("author");
-        LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
-
-        Array tagsArray = rs.getArray("tags");
-        List<String> tags = tagsArray == null
-                ? List.of()
-                : Arrays.asList((String[]) tagsArray.getArray());
-
-        return new PostResponseDTO(
-                (long) id, title, body,
-                author, tags, formatDate(updatedAt), totalComments
-        );
-    }
-
     public PostResponseDTO createResponseFromPostAndTags(Post post, String authorName, List<String> tags, Long totalComments) {
         return new PostResponseDTO(
-                post.getId(), post.getTitle(), post.getBody(), authorName, tags, formatDate(post.getUpdatedAt()), totalComments
+                post.getId(),
+                post.getTitle(),
+                post.getBody(),
+                String.valueOf(post.getAuthor().getId()),
+                authorName,
+                tags,
+                formatDate(post.getPostedAt()),
+                formatDate(post.getUpdatedAt()),
+                totalComments
         );
     }
 
@@ -66,9 +37,11 @@ public class PostUtils {
                 post.getTitle(),
                 post.getBody(),
                 post.getAuthor().getUsername(),
+                String.valueOf(post.getAuthor().getId()),
                 post.getTags().stream()
                         .map(Tag::getName)
                         .toList(),
+                formatDate(post.getPostedAt()),
                 formatDate(post.getUpdatedAt()),
                 totalComments
         );
