75d85fc9dfd30279a2a87797eb0f1bfeae6e5057
ograh <bernard.ograh@amalitech.com>
2026-02-11 11:59:46 +0000

    feat: implement CustomUserDetailsService for user authentication via Spring Security

---
+++ src/main/java/org/amalitech/bloggingplatformspring/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.amalitech.bloggingplatformspring.services;
+
+import lombok.RequiredArgsConstructor;
+import org.amalitech.bloggingplatformspring.entity.User;
+import org.amalitech.bloggingplatformspring.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmailIgnoreCase(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_USER")
+                .build();
+    }
+}
