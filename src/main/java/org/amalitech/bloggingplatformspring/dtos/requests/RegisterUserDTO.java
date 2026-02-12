f42895f7916277d840dd0ee6766cad4099463913
ograh <bernard.ograh@amalitech.com>
2026-02-12 15:35:12 +0000

    feat: enhance validation in RegisterUserDTO
    - Added regex constraints for `username` and `password` fields.
    - Updated validation messages for `email` and `password` fields.
    - Improved password validation to require uppercase, lowercase, and a digit.

--- src/main/java/org/amalitech/bloggingplatformspring/dtos/requests/RegisterUserDTO.java
+++ src/main/java/org/amalitech/bloggingplatformspring/dtos/requests/RegisterUserDTO.java
@@ -2,6 +2,7 @@ package org.amalitech.bloggingplatformspring.dtos.requests;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,13 +17,16 @@ public class RegisterUserDTO {
 
     @Size(min = 3, max = 36, message = "Username cannot be less than 3 or greater than 36")
     @NotBlank(message = "Username cannot be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9_-]+$", message = "Username can only contain letters, numbers, underscores and hyphens")
     private String username;
 
     @Email(message = "Invalid email format")
-    @NotBlank(message = "Email cannot be blank")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @Size(min = 6, max = 50, message = "Password cannot be less than 6 or greater than 50")
-    @NotBlank(message = "Password cannot be blank")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
+            message = "Password must contain uppercase, lowercase and digit")
     private String password;
 }
