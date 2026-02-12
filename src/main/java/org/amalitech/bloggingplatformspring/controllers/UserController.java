efad3378c79466689933665a977787e21146ac69
ograh <bernard.ograh@amalitech.com>
2026-02-12 08:25:19 +0000

    feat: add AuthController and AuthService for user authentication and registration
    - Implement `AuthController` with endpoints for user registration, sign-in, and token refresh.
    - Create `AuthService` to handle authentication logic and integrate it with token handling and user validation.
    - Refactor `UserController` to remove authentication-related methods, now managed in `AuthController`.
    - Enhance user profile retrieval in `UserService` with token-based validation.

--- src/main/java/org/amalitech/bloggingplatformspring/controllers/UserController.java
+++ src/main/java/org/amalitech/bloggingplatformspring/controllers/UserController.java
@@ -6,9 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.amalitech.bloggingplatformspring.dtos.requests.RegisterUserDTO;
-import org.amalitech.bloggingplatformspring.dtos.requests.SignInUserDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.amalitech.bloggingplatformspring.dtos.responses.ApiResponseGeneric;
 import org.amalitech.bloggingplatformspring.dtos.responses.UserProfileResponse;
 import org.amalitech.bloggingplatformspring.dtos.responses.UserResponseDTO;
@@ -16,68 +15,26 @@ import org.amalitech.bloggingplatformspring.exceptions.ErrorResponse;
 import org.amalitech.bloggingplatformspring.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
-@Tag(name = "1. User Management", description = "APIs for user registration and authentication")
+@Tag(name = "2. User Management", description = "APIs for user registration and authentication")
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping("/register")
-    @Operation(summary = "Register a new user", description = "Creates a new user account with the provided registration details. Returns the created user profile.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "User successfully registered", content = @Content(schema = @Schema(implementation = UserResponseDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Invalid input data or user already exists", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    })
-    public ResponseEntity<ApiResponseGeneric<UserResponseDTO>> registerUser(
-            @Valid @RequestBody RegisterUserDTO registerUserDTO) {
-        UserResponseDTO userResponse = userService.registerUser(registerUserDTO);
-        ApiResponseGeneric<UserResponseDTO> response = ApiResponseGeneric.success("User registration successful",
-                userResponse);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/sign-in")
-    @Operation(
-            summary = "Sign in a user",
-            description = "Authenticates a user with email and password. Returns the user profile upon successful authentication."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200", description = "User successfully signed in",
-                    content = @Content(schema = @Schema(implementation = UserResponseDTO.class))
-            ),
-            @ApiResponse(
-                    responseCode = "401", description = "Invalid credentials",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-            ),
-            @ApiResponse(
-                    responseCode = "404", description = "User not found",
-                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-            )
-    })
-    public ResponseEntity<ApiResponseGeneric<UserResponseDTO>> signInUser(
-            @Valid @RequestBody SignInUserDTO signInUserDTO) {
-        UserResponseDTO userResponse = userService.signInUser(signInUserDTO);
-        ApiResponseGeneric<UserResponseDTO> response = ApiResponseGeneric.success("User sign in successful",
-                userResponse);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @GetMapping("/profile/{userID}")
+    @GetMapping("/profile}")
     @Operation(
             summary = "Get User Profile",
             description = "Returns the user profile."
     )
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "200", description = "User profile returned successfully",
+                    responseCode = "200", description = "User profile retrieved successfully",
                     content = @Content(schema = @Schema(implementation = UserResponseDTO.class))
             ),
             @ApiResponse(
@@ -85,8 +42,8 @@ public class UserController {
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class))
             )
     })
-    public ResponseEntity<ApiResponseGeneric<UserProfileResponse>> getUserProfile(@PathVariable String userID) {
-        UserProfileResponse userResponse = userService.getUserProfile(userID);
+    public ResponseEntity<ApiResponseGeneric<UserProfileResponse>> getUserProfile(HttpServletRequest request) {
+        UserProfileResponse userResponse = userService.getUserProfile(request);
         ApiResponseGeneric<UserProfileResponse> response = ApiResponseGeneric.success("User profile retrieved successfully",
                 userResponse);
         return new ResponseEntity<>(response, HttpStatus.OK);
--- src/main/java/org/amalitech/bloggingplatformspring/services/UserService.java
+++ src/main/java/org/amalitech/bloggingplatformspring/services/UserService.java
@@ -1,38 +1,28 @@
 package org.amalitech.bloggingplatformspring.services;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.amalitech.bloggingplatformspring.dtos.requests.RegisterUserDTO;
-import org.amalitech.bloggingplatformspring.dtos.requests.SignInUserDTO;
 import org.amalitech.bloggingplatformspring.dtos.responses.CommentResponse;
 import org.amalitech.bloggingplatformspring.dtos.responses.PostResponseDTO;
 import org.amalitech.bloggingplatformspring.dtos.responses.UserProfileResponse;
-import org.amalitech.bloggingplatformspring.dtos.responses.UserResponseDTO;
 import org.amalitech.bloggingplatformspring.entity.Comment;
 import org.amalitech.bloggingplatformspring.entity.Post;
 import org.amalitech.bloggingplatformspring.entity.User;
 import org.amalitech.bloggingplatformspring.exceptions.BadRequestException;
 import org.amalitech.bloggingplatformspring.exceptions.InvalidUserIdFormatException;
-import org.amalitech.bloggingplatformspring.exceptions.ResourceNotFoundException;
 import org.amalitech.bloggingplatformspring.exceptions.UnauthorizedException;
 import org.amalitech.bloggingplatformspring.repository.CommentRepository;
 import org.amalitech.bloggingplatformspring.repository.PostRepository;
 import org.amalitech.bloggingplatformspring.repository.UserRepository;
+import org.amalitech.bloggingplatformspring.security.JwtTokenProvider;
 import org.amalitech.bloggingplatformspring.utils.CommentUtils;
-import org.amalitech.bloggingplatformspring.utils.Constants;
 import org.amalitech.bloggingplatformspring.utils.PostUtils;
 import org.amalitech.bloggingplatformspring.utils.UserUtils;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Limit;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
@@ -44,75 +34,20 @@ public class UserService {
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
     private final PostUtils postUtils;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserResponseDTO registerUser(RegisterUserDTO registerUserDTO) {
-        String username = registerUserDTO.getUsername();
-        String email = registerUserDTO.getEmail();
-        String password = registerUserDTO.getPassword();
-
-        if (password.toLowerCase().contains(username.toLowerCase())) {
-            throw new BadRequestException("Password must not contain username");
-        }
-
-        if (Boolean.TRUE.equals(userRepository.existsByUsernameIgnoreCase(username))) {
-            throw new BadRequestException("Username is taken");
-        }
-
-        if (Boolean.TRUE.equals(userRepository.existsByEmailIgnoreCase(email))) {
-            throw new BadRequestException("Email is taken");
-        }
-
-        String hashedPassword = passwordEncoder.encode(password);
-
-        User user = new User();
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword(hashedPassword);
-        userRepository.save(user);
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        return userUtils.mapUserToUserResponse(user);
-    }
-
-    public UserResponseDTO signInUser(SignInUserDTO signInUserDTO) {
-        String email = signInUserDTO.getEmail();
-        String password = signInUserDTO.getPassword();
-
-        if (Boolean.FALSE.equals(userRepository.existsByEmailIgnoreCase(email))) {
-            throw new UnauthorizedException("Invalid email or password");
-        }
-
+    public UserProfileResponse getUserProfile(HttpServletRequest httpServletRequest) {
+        String token = jwtTokenProvider.getTokenFromRequest(httpServletRequest);
+        String email = jwtTokenProvider.getEmailFromAccessToken(token);
         User user = userRepository.findUserByEmailIgnoreCase(email).orElseThrow(
                 () -> new UnauthorizedException("Invalid email or password")
         );
+        String userID = String.valueOf(user.getId());
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        return userUtils.mapUserToUserResponse(user);
-
-    }
-
-    @Cacheable(cacheNames = Constants.USERS_CACHE_NAME, key = "'profile:' + #userID")
-    public UserProfileResponse getUserProfile(String userID) {
         if (userID.isBlank())
             throw new BadRequestException("User ID cannot be empty");
 
         try {
-            UUID id = UUID.fromString(userID);
-            User user = userRepository.findById(id).orElseThrow(
-                    () -> new ResourceNotFoundException("User not found with id: " + id)
-            );
             List<Post> recentPosts = postRepository.findPostsByAuthorOrderByUpdatedAtDesc(user, Limit.of(4));
             List<PostResponseDTO> recentPostsResponse = recentPosts.stream()
                     .map(post -> {
