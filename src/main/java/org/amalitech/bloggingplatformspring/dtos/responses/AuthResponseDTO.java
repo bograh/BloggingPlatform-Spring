d84a0a41e593e90822eea2368217e2539a901752
ograh <bernard.ograh@amalitech.com>
2026-02-12 08:23:36 +0000

    feat: add AuthResponse and AuthResponseDTO for authentication response handling

---
+++ src/main/java/org/amalitech/bloggingplatformspring/dtos/responses/AuthResponseDTO.java
@@ -0,0 +1,15 @@
+package org.amalitech.bloggingplatformspring.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String refreshToken;
+    private AuthResponse authResponse;
+}
