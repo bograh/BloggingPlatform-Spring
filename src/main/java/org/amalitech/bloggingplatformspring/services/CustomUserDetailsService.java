edeed9a97328cb67449d9c495658d090488b06fb
ograh <bernard.ograh@amalitech.com>
2026-02-13 08:21:44 +0000

    feat: enhance security and role-based access handling
    - Introduced `JwtAccessDeniedHandler` to handle access denial with detailed error responses.
    - Updated `SecurityConfig` to include `accessDeniedHandler` for better exception management.
    - Enhanced `CustomUserDetailsService` to map user roles dynamically into granted authorities.

--- src/main/java/org/amalitech/bloggingplatformspring/services/CustomUserDetailsService.java
+++ src/main/java/org/amalitech/bloggingplatformspring/services/CustomUserDetailsService.java
@@ -2,11 +2,17 @@ package org.amalitech.bloggingplatformspring.services;
 
 import lombok.RequiredArgsConstructor;
 import org.amalitech.bloggingplatformspring.entity.User;
+import org.amalitech.bloggingplatformspring.enums.UserRoles;
 import org.amalitech.bloggingplatformspring.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -19,10 +25,16 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findUserByEmailIgnoreCase(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
+        List<UserRoles> userRoles = user.getUserRoles();
+
+        List<GrantedAuthority> authorities = userRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
-                .authorities("ROLE_USER")
+                .authorities(authorities)
                 .build();
     }
 }
